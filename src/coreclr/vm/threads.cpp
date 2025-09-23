01be384b3938237baa0186f40d3f5324b980ebbc
Jan Kotas <jkotas@microsoft.com>
2025-09-22 22:25:46 -0700

    Delete unused EnsureComStarted flag

--- src/coreclr/vm/threads.cpp
+++ src/coreclr/vm/threads.cpp
@@ -4796,10 +4796,6 @@ Thread::ApartmentState Thread::SetApartment(ApartmentState state)
         }
     }
 
-    // Since we've just called CoInitialize, COM has effectively been started up.
-    // To ensure the CLR is aware of this, we need to call EnsureComStarted.
-    EnsureComStarted(FALSE);
-
     return GetApartment();
 }
 
